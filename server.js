2dd6b3210b8d9310fa01a96fa21f6b42c1c9f867
Tyler Casperson <tylercasperson@gmail.com>
2019-12-13 23:40:33 -0700

    testing heroku deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
+// Handlebars   
 app.engine(
   "handlebars",
   exphbs({
