245df72962b1caf2041638cd292b88ef80789812
Tyler Casperson <tylercasperson@gmail.com>
2019-12-20 02:40:40 -0700

    added the table to the landing page

--- public/js/index.js
+++ public/js/index.js
@@ -1,121 +1,97 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveForecastData: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/salesHistory",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
+    // Get references to page elements
+    var $mainCategory = $("#mainCategory");
+    var $subCategory = $("subCategpry");
+    var $categoryYear = $("categoryYear");
+    var $january = $("#january");
+    var $february = $("#february ");
+    var $march = $("#march");
+    var $april = $("#april");
+    var $may = $("#may");
+    var $june = $("#june");
+    var $july = $("#july");
+    var $august = $("#august");
+    var $september = $("#september");
+    var $october = $("#october");
+    var $november = $("#november");
+    var $december = $("#december");
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+  // The API object contains methods for each kind of request we'll make
+  var API = {
+    saveSalesHistory: function(id) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/salesHistory/" + id,
+        data: JSON.stringify(salesHistory)
+      });
+    },
+    getSalesHistory: function() {
+      return $.ajax({
+        url: "api/salesHistory",
+        type: "GET"
+      });
+    },
+    deleteSalesHistory: function(id) {
+      return $.ajax({
+        url: "api/salesHistory/" + id,
+        type: "DELETE"
+      });
+    }
   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+  // refreshExamples gets new examples from the db and repopulates the list
+  var refreshData = function() {
+    API.getSalesHistory().then(function(data) {
+      var $salesHistory = data.map(function(salesHistory) {
+        var $january = $("#january")
+          .text(salesHistory.SalesData)
+          .prepend($january);
+          console.log("1");
+        return $january;
+      });
 
 
-  // Function for creating a new list row for authors
-  function createSalesHistoryRow(salesHistory) {
-    console.log(salesHistory);
-    var newTr = $("<tr>");
-    newTr.data("salesData", salesHistory);
-    newTr.append("<td>" + salesHistory.SalesData + "</td>");
-    return newTr;
-  };
-
-  // Function for retrieving authors and getting them ready to be rendered to the page
-  function getSalesHistory() {
-    $.get("/api/salesHistorys", function(data) {
-      var rowsToAdd = [];
-      for (var i = 0; i < data.length; i++) {
-        rowsToAdd.push(createSalesHistoryRow(data[i]));
-      }
-      renderSalesHistoryList(rowsToAdd);
-      nameInput.val("");
     });
   };
+
+  // handleFormSubmit is called whenever we submit a new example
+  // Save the new example to the db and refresh the list
+  var handleFormSubmit = function(event) {
+    event.preventDefault();
+
+    var example = {
+      text: $exampleText.val().trim(),
+      description: $exampleDescription.val().trim()
+    };
+
+    if (!(example.text && example.description)) {
+      alert("You must enter an example text and description!");
+      return;
+    }
+
+    API.saveExample(example).then(function() {
+      refreshExamples();
+    });
+
+    $exampleText.val("");
+    $exampleDescription.val("");
+  };
+
+  // handleDeleteBtnClick is called when an example's delete button is clicked
+  // Remove the example from the db and refresh the list
+  var handleDeleteBtnClick = function() {
+    var idToDelete = $(this)
+      .parent()
+      .attr("data-id");
+
+    API.deleteExample(idToDelete).then(function() {
+      refreshExamples();
+    });
+  };
+
+  // Add event listeners to the submit and delete buttons
+  $submitBtn.on("click", handleFormSubmit);
+  $exampleList.on("click", ".delete", handleDeleteBtnClick);
