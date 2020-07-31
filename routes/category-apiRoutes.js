bf4e31390c8252ffa0d04cb9cdfc975bc86ff0e2
Tyler Casperson <tylercasperson@gmail.com>
2020-07-30 23:03:33 -0600

    starting to convert to a react application

--- routes/category-apiRoutes.js
+++
@@ -1,9 +0,0 @@
-var db = require("../models");
-
-module.exports = function(app) {
-  app.get("/api/category", function(req, res) {
-    db.category.findAll({}).then(function(ForecastToolDB) {
-      res.json(ForecastToolDB);
-    });
-  });
-};
--- routes/forecastMethod-apiRoutes.js
+++
@@ -1,9 +0,0 @@
-var db = require("../models");
-
-module.exports = function(app) {
-  app.get("/api/forecastMethod", function(req, res) {
-    db.forecastMethod.findAll({}).then(function(ForecastToolDB) {
-      res.json(ForecastToolDB);
-    });
-  });
-};
